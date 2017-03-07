042ba71efb13e424cb2ba3c5d6a6e45cf9a348c7
jeff1st <jeff1st@gmail.com>
2017-03-07 22:00:08 +0100

    Prepraring to push to Github

--- merge_s.rb
+++ merge_s.rb
@@ -1,6 +1,3 @@
-input_array1 = [23, 43, 1, 45, 76, 789, 474, 3, 98, 34, 23, 5, 9, 987, 1298, 3]
-input_array2 = [3, 143, 11, 435, 26, 79, 274, 37, 981, 14, 231, 55, 95, 7, 129, 32, 45, 7878]
-
 def merge_sort(array)
   if array.length == 1
     return array
@@ -21,8 +18,3 @@ def merge_sort(array)
     return new_array
   end
 end
-
-new_array1 = merge_sort(input_array1)
-puts new_array1.inspect
-new_array2 = merge_sort(input_array2)
-puts new_array2.inspect
